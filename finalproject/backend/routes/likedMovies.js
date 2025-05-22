a0d8a01a05ecd5c241b2ed7c841ae7afce8d3d9c
hadimohammed17 <hmoham17@terpmail.umd.edu>
2025-05-22 15:18:54 -0400

    Add files via upload

---
+++ finalproject/backend/routes/likedMovies.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const supabase = require('../supabase');
+
+// test route
+router.get('/test', (req, res) => {
+  res.json({ message: 'likedMovies test works' });
+});
+
+// POST /api/saved — save a movie
+router.post('/', async (req, res) => {
+  const { title, overview } = req.body;
+
+  const { data, error } = await supabase
+    .from('likedMovies')
+    .insert([{ title, overview }]);
+
+  if (error) {
+    console.error('Supabase insert error:', error.message);
+    return res.status(500).json({ error: error.message });
+  }
+
+  res.status(200).json({ message: 'Movie saved!', data });
+});
+
+// retrives saved movies
+router.get('/', async (req, res) => {
+  const { data, error } = await supabase
+    .from('likedMovies')
+    .select('*');
+
+  if (error) {
+    console.error('Supabase fetch error:', error.message);
+    return res.status(500).json({ error: error.message });
+  }
+
+  res.status(200).json(data);
+});
+
+module.exports = router;

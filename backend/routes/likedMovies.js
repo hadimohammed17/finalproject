19838ad23786f59be8fa9015ec86daff215daef8
hadimohammed17 <hmoham17@terpmail.umd.edu>
2025-05-23 00:43:31 -0400

    likedMovies.js

--- backend/routes/likedMovies.js
+++ backend/routes/likedMovies.js
@@ -2,36 +2,35 @@ const express = require('express');
 const router = express.Router();
 const supabase = require('../supabase');
 
-// test route
+// test endpoint to make sure it's working
 router.get('/test', (req, res) => {
   res.json({ message: 'likedMovies test works' });
 });
 
-// POST /api/saved — save a movie
+// save a movie to supabase
 router.post('/', async (req, res) => {
-  const { title, overview } = req.body;
+  const { title, overview, genres } = req.body;
 
   const { data, error } = await supabase
     .from('likedMovies')
-    .insert([{ title, overview }]);
+    .insert([{ title, overview, genres }]);
 
   if (error) {
-    console.error('Supabase insert error:', error);
-
+    console.error('supabase insert error:', error);
     return res.status(500).json({ error: error.message });
   }
 
-  res.status(200).json({ message: 'Movie saved!', data });
+  res.status(200).json({ message: 'movie saved!', data });
 });
 
-// retrives saved movies
+// get all saved movies
 router.get('/', async (req, res) => {
   const { data, error } = await supabase
     .from('likedMovies')
     .select('*');
 
   if (error) {
-    console.error('Supabase fetch error:', error.message);
+    console.error('supabase fetch error:', error.message);
     return res.status(500).json({ error: error.message });
   }
 
